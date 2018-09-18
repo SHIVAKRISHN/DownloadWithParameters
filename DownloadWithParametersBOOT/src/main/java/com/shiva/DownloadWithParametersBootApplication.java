9b4aeba721deefec56bf48294c43e19c35d6c557
SHIVA <shiva1607502.office@gmail.com>
2018-09-18 15:55:22 +0530

    Initial Commit

---
+++ DownloadWithParametersBOOT/src/main/java/com/shiva/DownloadWithParametersBootApplication.java
@@ -0,0 +1,12 @@
+package com.shiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DownloadWithParametersBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DownloadWithParametersBootApplication.class, args);
+	}
+}
